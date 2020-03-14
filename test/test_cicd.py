b5369507e49120135d19c689daf5f9125ab65b57
oommen81 <jomon_george@msn.com>
2020-03-13 22:20:18 -0400

    test

--- test/test_cicd.py
+++ test/test_cicd.py
@@ -7,15 +7,16 @@ class MyTestCase(unittest.TestCase):
     def __init__(self):
         self.obj = cicd.cicd()
     """
-
+    def setUp(self) -> None:
+        self.obj= cicd.cicd()
     def test_something(self):
-        obj = cicd.cicd()
-        ret =obj.process_count(1,2)
+        #obj = cicd.cicd()
+        ret =self.obj.process_count(1,2)
         self.assertEqual(ret,3)
     def test_multi(self):
-        obj = cicd.cicd()
-        mult = obj.process_multi(1,2)
-        self.assertEqual(mult,1,"multi")
+        #obj = cicd.cicd()
+        mult = self.obj.process_multi(1,2)
+        self.assertEqual(mult,2,"multi")
 
 
 if __name__ == '__main__':
