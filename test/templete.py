f4a9aa4ce95bcf4745e77962aeb549da6c3c98a0
oommen81 <jomon_george@msn.com>
2020-03-16 11:58:52 -0400

    user story

---
+++ test/templete.py
@@ -0,0 +1,30 @@
+import unittest
+from src import cicd
+
+
+
+class MyTestCase(unittest.TestCase):
+    """
+    def __init__(self):
+        self.obj = cicd.cicd()
+    """
+    def setUp(self) -> None:
+        self.obj= cicd.cicd()
+    def test_something(self):
+        #obj = cicd.cicd()
+        print("test1")
+        ret =self.obj.process_count(1,2)
+        self.assertEqual(ret,3)
+    def test_multi(self):
+        #obj = cicd.cicd()
+        mult = self.obj.process_multi(1,2)
+        self.assertEqual(mult,2,"multi")
+
+
+if __name__ == '__main__':
+    suite = unittest.TestSuite()
+    testfunctions = ['test_something','test_multi']
+    for fns in testfunctions :
+        suite.addTest(MyTestCase(fns))    #unittest.main()
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
