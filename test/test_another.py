4230bdfb6441d2f232bd8be304c37d2a7d734a51
oommen81 <jomon_george@msn.com>
2020-03-16 11:42:54 -0400

    user story

---
+++ test/test_another.py
@@ -0,0 +1,24 @@
+import unittest
+from src import cicd
+
+
+
+class MyTestCase(unittest.TestCase):
+    """
+    def __init__(self):
+        self.obj = cicd.cicd()
+    """
+    def setUp(self) -> None:
+        self.obj= cicd.cicd()
+    def test_something(self):
+        #obj = cicd.cicd()
+        ret =self.obj.process_count(1,2)
+        self.assertEqual(ret,3)
+    def test_multi(self):
+        #obj = cicd.cicd()
+        mult = self.obj.process_multi(1,2)
+        self.assertEqual(mult,2,"multi")
+
+
+if __name__ == '__main__':
+    unittest.main()
